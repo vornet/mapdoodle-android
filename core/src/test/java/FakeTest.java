d55970ed6e4b59e7062b0121882cb604245a8df7
Vorn Mom <vorn@vorn.us>
2016-07-27 11:54:13 -0400

    Fake unit test.

---
+++ core/src/test/java/FakeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Created by Vorn on 7/17/2016.
+ */
+public class FakeTest {
+    @Test
+    public void fakeTest_True_IndeedTrue() {
+        assertThat(true, is(true));
+    }
+}
